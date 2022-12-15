50ca0b01e81c9d35bf31097d41196b5d020302c6
Shivam Nahar <33218367+imsherlocked@users.noreply.github.com>
2022-12-14 20:48:35 -0500

    Minor changes
    Signed-off-by: Shivam Nahar <33218367+imsherlocked@users.noreply.github.com>

--- map.js
+++ map.js
@@ -1,16 +1,22 @@
 var map = L.map("map").setView([39.324, -76.612], 12);
-
 L.esri.Vector.vectorBasemapLayer("ArcGIS:Community", {
   apikey:
     "AAPK8259af609fad4afaad0554ebf97205c9ps9yDjujF4N1L4UjHBywwT-yYLQiyUni7Sca8I_7F5k9tJ6zImhOpMi4Pg62uX7w",
 }).addTo(map);
-
 var treesFeatureLayer = L.esri
   .featureLayer({
     url: "https://services3.arcgis.com/bO7Hf5i6aZdITjmq/arcgis/rest/services/final_data/FeatureServer/0",
   })
   .addTo(map);
-
+treesFeatureLayer.eachFeature(function (e) {
+  e.setOpacity(1);
+});
+treesFeatureLayer.bindPopup(function (layer) {
+  return L.Util.template(
+    "<p>Frequently occurred Crime:<strong>{Description}</strong><br>Crime Year<strong>{CrimeDate}</strong><br>location:<strong>{Neighbourhood}</strong></p>",
+    layer.feature.properties
+  );
+});
 var initialChartData = {
   datasets: [
     {
@@ -19,7 +25,6 @@ var initialChartData = {
     },
   ],
 };
-
 var chartOptions = {
   scales: {
     xAxes: [
@@ -55,54 +60,44 @@ var chartOptions = {
   },
   onHover: handleChartHover,
 };
-
 var chart = new Chart("chartCanvas", {
   type: "scatter",
   data: initialChartData,
   options: chartOptions,
 });
-
 map.on("zoom move", updateChart);
 treesFeatureLayer.on("load", updateChart);
-
 function updateChart() {
   var scatterPlotDataArray = [];
-
   treesFeatureLayer.eachActiveFeature(function (e) {
-    scatterPlotDataArray.push({
-      x: e.feature.properties.Impact,
-      y: e.feature.properties.Intensity,
-      featureId: e.feature.id,
-    });
+    if (!(e.options.opacity == 0)) {
+      scatterPlotDataArray.push({
+        x: e.feature.properties.Impact,
+        y: e.feature.properties.Intensity,
+        featureId: e.feature.id,
+      });
+    }
   });
-
   chart.data.datasets[0].data = scatterPlotDataArray;
-
   chart.update();
 }
-
 // STEP 4 : ESTABLISH CHART-TO-MAP COMMUNICATION
-
 function handleChartHover(e) {
   var chartHoverData = chart.getElementsAtEvent(e);
-
   if (!chartHoverData.length) {
     treesFeatureLayer.eachFeature(function (e) {
       e.setOpacity(1);
       e.setZIndexOffset(0);
     });
-
     return;
   }
-
   var hoverFeatureIds = chartHoverData.map(function (datum) {
     return chart.data.datasets[0].data[datum._index].featureId;
   });
-
   treesFeatureLayer.eachFeature(function (e, idx) {
     if (hoverFeatureIds.indexOf(e.feature.id) > -1) {
       e.setOpacity(1);
-      e.setZIndexOffset(1000);
+      e.setZIndexOffset(10);
     } else {
       e.setOpacity(0.01);
     }
